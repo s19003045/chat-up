ff438d8df897962f020b1a49d466806829231db8
Gary Wu <s19003045@gmail.com>
2020-03-28 18:52:03 +0800

    建立 web-socket 之連線、傳送訊息及接收訊息功能，及聊天室頁面

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
+// 判別開發環境
+if (process.env.NODE_ENV !== "production") {
+  // 如果不是 production 模式，使用 dotenv 讀取 .env 檔案
+  require("dotenv").config();
+}
+
+const port = process.env.PORT || 3000
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// view engine
+const exphbs = require('express-handlebars')
+
+// view engine setting
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+// use public file
+app.use(express.static("public"));
+
+// route setting
 app.get('/', (req, res) => {
-  console.log('index page')
+  return res.render('chatroom')
 })
 
-app.listen(port, () => {
+app.use('*', (req, res) => {
+  return res.redirect('/')
+})
+
+// For websocket
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // 事件：使用者傳送訊息
+  socket.on('client chat', function (data) {
+    console.log('client message:', data)
+
+    // 發送訊息給所有人
+    io.emit('server read', data);
+  })
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+// Server listen
+server.listen(port, () => {
   console.log(`Express app listening on port ${port}!`)
 });
